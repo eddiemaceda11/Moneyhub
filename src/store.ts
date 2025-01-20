f2f241a515a28e386b3431fe41b05343f9e4c0dd
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-19 20:36:21 -0700

    Add the overviewApi to the global store

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { overviewApi } from "./services/overviewApi";
+
 import overviewReducer from "./features/overview/overviewSlice";
 
 export const store = configureStore({
   reducer: {
     overview: overviewReducer,
+    [overviewApi.reducerPath]: overviewApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(overviewApi.middleware),
 });
