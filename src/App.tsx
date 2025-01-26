f83c1029432c1fd072d5a217d34fea70926d4c8b
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-25 18:58:33 -0700

    Add a container div for the main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css'
-import { Sidebar } from './features/sidebar/Sidebar';
-import { Overview } from './features/overview/Overview';
+import "./App.css";
+import { Sidebar } from "./features/sidebar/Sidebar";
+import { Overview } from "./features/overview/Overview";
 
 function App() {
   return (
     <div className="page-container">
-     <Sidebar />
-     <main>
-      {/* <div className='main-container'> */}
-        <Overview />
-      {/* </div> */}
-     </main>
-    </div> 
-  )
+      <Sidebar />
+      <main>
+        <div className="main-container">
+          <Overview />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
