989dd934ea206a8d035fba8f1cb0190b17d7d45c
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-25 17:35:51 -0700

    Create the html layout with temporary data for the Oversection Recurring Bills Main content section

--- src/features/overview/Overview.tsx
+++ src/features/overview/Overview.tsx
@@ -264,6 +264,20 @@ export const Overview = () => {
                 <span>x</span>
               </div>
             </div>
+            <div className="osr-content">
+              <div>
+                <p>Paid Bills</p>
+                <span>$190.00</span>
+              </div>
+              <div>
+                <p>Total Upcoming</p>
+                <span>$194.98</span>
+              </div>
+              <div>
+                <p>Due Soon</p>
+                <span>$59.48</span>
+              </div>
+            </div>
           </div>
         </div>
       </section>
