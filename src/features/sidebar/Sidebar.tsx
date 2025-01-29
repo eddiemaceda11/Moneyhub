03439549219a5b66d32fab05fe2467a9ece509e1
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-28 23:34:24 -0700

    Import the Sidebar overview icon

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import "./sidebar.css";
+import sidebarOverviewIcon from "../../assets/images/icon-nav-overview.svg";
 
 export const Sidebar = () => {
   return (
@@ -7,7 +8,12 @@ export const Sidebar = () => {
         <h2>moneyhub</h2>
       </header>
       <nav className="sidebar-nav">
-        <div className="sidebar-nav-item"></div>
+        <div className="sidebar-nav-item">
+          <img
+            src={sidebarOverviewIcon}
+            alt=""
+          />
+        </div>
         <div className="sidebar-nav-item"></div>
         <div className="sidebar-nav-item"></div>
         <div className="sidebar-nav-item"></div>
