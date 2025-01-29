29694b5420414f8dec909c3fb3f83517e65d2eb8
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-28 23:15:25 -0700

    Create the Sidebar header

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -2,6 +2,10 @@ import "./sidebar.css";
 
 export const Sidebar = () => {
   return (
-    <div className="sidebar">Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <header className="sidebar-header">
+        <h2>moneyhub</h2>
+      </header>
+    </div>
+  );
+};
