bf4fbe339948f4725231b743c564d9c3f1ac6230
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-28 22:22:32 -0700

    Refactor the useBalanceData hook

---
+++ src/hooks/useBalanceData.ts
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../utils/formatters";
+
+interface Balance {
+  current: string;
+  expenses: string;
+  income: string;
+}
+
+export const useBalanceData = (balance?: Balance) => {
+  if (!balance) {
+    return { current: "0.00", expenses: "0.00", income: "0.00" };
+  }
+
+  return {
+    current: formatCurrency(balance.current),
+    expenses: formatCurrency(balance.expenses),
+    income: formatCurrency(balance.income),
+  };
+};
