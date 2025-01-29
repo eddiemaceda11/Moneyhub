77060b0e723806e838b676c9db120987b098c630
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-28 22:49:25 -0700

    Create the usePotsData hook

---
+++ src/hooks/usePotsData.ts
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../utils/formatters";
+
+type Pots = {
+  name: string;
+  target: string;
+  theme: string;
+  total: string;
+};
+
+export const usePotsData = (pots?: Pots[]) => {
+  if (!pots) {
+    return [
+      { name: "", target: 0, theme: "", total: 0 },
+      { name: "", target: 0, theme: "", total: 0 },
+      { name: "", target: 0, theme: "", total: 0 },
+      { name: "", target: 0, theme: "", total: 0 },
+    ];
+  }
+
+  const formattedPots: Pots[] = pots.map((potObject) => {
+    return {
+      name: potObject.name,
+      target: formatCurrency(potObject.target.toString()),
+      theme: potObject.theme,
+      total: formatCurrency(potObject.total.toString()),
+    };
+  });
+
+  return formattedPots;
+};
