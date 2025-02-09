b7026ec9f8be404c5321d401b3860d1565c16c40
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 12:51:32 -0700

    Remove the configureImgPath function from the OverviewTransaction file

--- src/features/overview/components/OverviewTransactions/OverviewTransaction.tsx
+++ src/features/overview/components/OverviewTransactions/OverviewTransaction.tsx
@@ -2,11 +2,6 @@ import { IndividualTransaction } from "./IndividualTransaction";
 import emmaImage from "/images/avatars/emma-richardson.jpg";
 
 export const OverviewTransactions = ({ transactionsData }: any) => {
-  // TODO -> move into a utils folder
-  function configureImgPath(image: string): string {
-    return image.slice(8);
-  }
-
   const overviewTransactions = transactionsData.slice(0, 5);
 
   const renderedOverviewTransactions = overviewTransactions.map((transaction: any) => {
