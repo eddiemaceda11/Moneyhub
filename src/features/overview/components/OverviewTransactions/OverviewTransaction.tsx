6cfa6ae231bfefa07f3851e46e5847e6ef69b9fd
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 12:03:39 -0700

    Import the IndividualTransaction component and pass the transactionsData through as props

--- src/features/overview/components/OverviewTransactions/OverviewTransaction.tsx
+++ src/features/overview/components/OverviewTransactions/OverviewTransaction.tsx
@@ -1,3 +1,4 @@
+import { IndividualTransaction } from "./IndividualTransaction";
 import emmaImage from "/images/avatars/emma-richardson.jpg";
 
 export const OverviewTransactions = ({ transactionsData }: any) => {
@@ -16,21 +17,7 @@ export const OverviewTransactions = ({ transactionsData }: any) => {
         </div>
       </header>
       <div className="overview-transactions-info">
-        <div>
-          <div className="oti-left">
-            <img
-              src={configureImgPath(transactionsData[0].avatar)}
-              alt=""
-            />
-            <p>{transactionsData[0].name}</p>
-          </div>
-          <div className="oti-right">
-            {/* <p>+{transactionsData[0].amount}</p> */}
-            <p>+79.80</p>
-            <span>19 Aug 2024</span>
-            {/* <span>{transactionsData[0].date}</span> */}
-          </div>
-        </div>
+        <IndividualTransaction transactionsData={transactionsData} />
         <div className="oti-line"></div>
         <div>
           <div className="oti-left">
