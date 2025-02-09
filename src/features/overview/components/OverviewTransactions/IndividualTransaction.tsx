75800aa9aa8bfe6c9e63d8644828bad811a02234
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 12:01:49 -0700

    Configure the IndividualTransaction component to render a single transaction div

--- src/features/overview/components/OverviewTransactions/IndividualTransaction.tsx
+++ src/features/overview/components/OverviewTransactions/IndividualTransaction.tsx
@@ -1,3 +1,24 @@
-export const IndividualTransaction = () => {
-  return <div>Individual Transaction</div>;
+export const IndividualTransaction = ({ transactionsData }: any) => {
+  // TODO -> move into a utils folder
+  function configureImgPath(image: string): string {
+    return image.slice(8);
+  }
+
+  return (
+    <div>
+      <div className="oti-left">
+        <img
+          src={configureImgPath(transactionsData[0].avatar)}
+          alt=""
+        />
+        <p>{transactionsData[0].name}</p>
+      </div>
+      <div className="oti-right">
+        {/* <p>+{transactionsData[0].amount}</p> */}
+        <p>+79.80</p>
+        <span>19 Aug 2024</span>
+        {/* <span>{transactionsData[0].date}</span> */}
+      </div>
+    </div>
+  );
 };
