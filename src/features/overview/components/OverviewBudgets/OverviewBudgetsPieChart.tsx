d89d6f7179cf1f3ae067e18f6e86f4727f6b0b76
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 13:02:58 -0700

    Create and configure the OverviewBudgetsPieChart component

---
+++ src/features/overview/components/OverviewBudgets/OverviewBudgetsPieChart.tsx
@@ -0,0 +1,14 @@
+export const OverviewBudgetsPieChart = () => {
+  return (
+    <div className="outer-pie-graph">
+      <div className="middle-pie-graph">
+        <div className="inner-pie-graph">
+          <div className="ipg-data">
+            <h4>$338</h4>
+            <p>of $500 limit</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
