af0f2c4e95c7cbe12bb1a7e6b9c8a6e6d5e305f0
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 12:37:35 -0700

    Remove extra comments and add the .oti-line div

--- src/features/overview/components/OverviewTransactions/IndividualTransaction.tsx
+++ src/features/overview/components/OverviewTransactions/IndividualTransaction.tsx
@@ -4,21 +4,24 @@ export const IndividualTransaction = ({ transactionsData }: any) => {
     return image.slice(8);
   }
 
+  console.log("transactionsData:", transactionsData);
+
   return (
-    <div>
-      <div className="oti-left">
-        <img
-          src={configureImgPath(transactionsData[0].avatar)}
-          alt=""
-        />
-        <p>{transactionsData[0].name}</p>
+    <>
+      <div>
+        <div className="oti-left">
+          <img
+            src={configureImgPath(transactionsData.avatar)}
+            alt=""
+          />
+          <p>{transactionsData.name}</p>
+        </div>
+        <div className="oti-right">
+          <p>+79.80</p>
+          <span>19 Aug 2024</span>
+        </div>
       </div>
-      <div className="oti-right">
-        {/* <p>+{transactionsData[0].amount}</p> */}
-        <p>+79.80</p>
-        <span>19 Aug 2024</span>
-        {/* <span>{transactionsData[0].date}</span> */}
-      </div>
-    </div>
+      {transactionsData.name === "Urban Services Hub" ? "" : <div className="oti-line"></div>}
+    </>
   );
 };
