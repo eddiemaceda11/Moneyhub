1808bbc3b3dd03de258d51c0f60461eed339a925
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 22:41:33 -0700

    Create the sort by filters dropdown

--- src/features/transactions/Transactions.tsx
+++ src/features/transactions/Transactions.tsx
@@ -13,6 +13,17 @@ export const Transaction = () => {
           <div className="filters-container">
             <div className="transactions_sort-by">
               <p>Sort by</p>
+              <select
+                name=""
+                id=""
+              >
+                <option value="latest">Latest</option>
+                <option value="oldest">Oldest</option>
+                <option value="az">A to Z</option>
+                <option value="za">Z to A</option>
+                <option value="highest">Highest</option>
+                <option value="lowest">Lowest</option>
+              </select>
             </div>
           </div>
         </div>
