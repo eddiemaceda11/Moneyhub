2a8545c92e1baa071fa71e65c4f9bec994ea35f5
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 21:22:00 -0700

    Setup the BrowserRouter and Routes components from react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { Sidebar } from "./features/sidebar/Sidebar";
 import { Overview } from "./features/overview/Overview";
+import { Transaction } from "./features/transactions/Transactions";
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <Sidebar />
       <main>
         <div className="main-container">
-          <Overview />
+          <BrowserRouter>
+            <Routes></Routes>
+          </BrowserRouter>
         </div>
       </main>
       {/* <Sidebar /> */}
