f3c0e0c01635c39a79c22b0e274e26150389a161
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 20:51:50 -0700

    Create and setup the initial configuration of the OverviewPots component

---
+++ src/features/overview/components/OverviewPots/OverviewPots.tsx
@@ -0,0 +1,55 @@
+import { JarIcon } from "./JarIcon";
+
+export const OverviewPots = ({ potsData }: any) => {
+  return (
+    <div className="overview-section-pots">
+      <div className="overview-pots-header">
+        <h3>Pots</h3>
+        <p>See Details</p>
+      </div>
+      <div className="overview-pots-info">
+        <div className="ov-pots-total-saved">
+          <JarIcon />
+          <div className="opts-info">
+            <p>Total Saved</p>
+            <span>$850</span>
+          </div>
+        </div>
+        <div className="ov-pots-info-boxes">
+          <div className="opib-top">
+            <div className="opib-top-left">
+              <div className="opib-border one"></div>
+              <div className="opib-items">
+                <p>{potsData[1].name}</p>
+                <span>${potsData[1].total}</span>
+              </div>
+            </div>
+            <div className="opib-top-right">
+              <div className="opib-border two"></div>
+              <div className="opib-items">
+                <p>{potsData[2].name}</p>
+                <span>${potsData[2].total}</span>
+              </div>
+            </div>
+          </div>
+          <div className="opib-bottom">
+            <div className="opib-bottom-left">
+              <div className="opib-border three"></div>
+              <div className="opib-items">
+                <p>{potsData[3].name}</p>
+                <span>${potsData[3].total}</span>
+              </div>
+            </div>
+            <div className="opib-bottom-right">
+              <div className="opib-border four"></div>
+              <div className="opib-items">
+                <p>{potsData[4].name}</p>
+                <span>${potsData[4].total}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
