8c0d53193e58305b17200ec9847175ed40012304
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-11 20:44:52 -0700

    Create the Transaction type

--- src/hooks/useTransactionsData.ts
+++ src/hooks/useTransactionsData.ts
@@ -1,1 +1,10 @@
 import { formatCurrency } from "../utils/formatters";
+
+type Transaction = {
+  amount: string;
+  avatar: string;
+  category: string;
+  date: string;
+  name: string;
+  recurring: boolean;
+};
