80bb3d778b6bd7f69e64710de529504c8a9c04fd
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-11 20:45:27 -0700

    Create and configure the useTransactionsData hook

--- src/hooks/useTransactionsData.ts
+++ src/hooks/useTransactionsData.ts
@@ -1,5 +1,4 @@
 import { formatCurrency } from "../utils/formatters";
-
 type Transaction = {
   amount: string;
   avatar: string;
@@ -7,4 +6,29 @@ type Transaction = {
   date: string;
   name: string;
   recurring: boolean;
+};
+
+export const useTransactionsData = (transactions?: Transaction[]) => {
+  console.log("trans: ", transactions);
+  if (!transactions)
+    return {
+      amount: 0,
+      avatar: "",
+      category: "None",
+      date: "",
+      name: "",
+      recurring: false,
+    };
+
+  const formattedTransactions = transactions.map((transactionObject) => {
+    return {
+      amount: formatCurrency(transactionObject.amount),
+      avatar: transactionObject.avatar,
+      category: transactionObject.category,
+      date: transactionObject.date,
+      name: transactionObject.name,
+      recurring: transactionObject.recurring,
+    };
+  });
+  return formattedTransactions;
 };
