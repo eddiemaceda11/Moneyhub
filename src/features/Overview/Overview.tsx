8c6bfd89ed1a205d91cba19902d28c9844e05f93
Eddie Maceda <eddiemaceda11@gmail.com>
2025-04-15 13:41:54 -0700

    Remove console.log from Overview.tsx

--- src/features/Overview/Overview.tsx
+++ src/features/Overview/Overview.tsx
@@ -1,21 +1,20 @@
-import "./overview.css";
+import './overview.css';
 
-import { useGetOverviewQuery } from "../../services/overviewApi";
+import { useGetOverviewQuery } from '../../services/overviewApi';
 
-import { useBalanceData } from "../../hooks/useBalanceData";
-import { usePotsData } from "../../hooks/usePotsData";
-import { useTransactionsData } from "../../hooks/useTransactionsData";
-import { useBudgetData } from "../../hooks/useBudgetData";
+import { useBalanceData } from '../../hooks/useBalanceData';
+import { usePotsData } from '../../hooks/usePotsData';
+import { useTransactionsData } from '../../hooks/useTransactionsData';
+import { useBudgetData } from '../../hooks/useBudgetData';
 
-import { OverviewTransactions } from "./components/OverviewTransactions/OverviewTransaction";
-import { OverviewBudgets } from "./components/OverviewBudgets/OverviewBudgets";
-import { OverviewPots } from "./components/OverviewPots/OverviewPots";
-import { OverviewRecurring } from "./components/OverviewRecurring/OverviewRecurring";
+import { OverviewTransactions } from './components/OverviewTransactions/OverviewTransaction';
+import { OverviewBudgets } from './components/OverviewBudgets/OverviewBudgets';
+import { OverviewPots } from './components/OverviewPots/OverviewPots';
+import { OverviewRecurring } from './components/OverviewRecurring/OverviewRecurring';
 
 export const Overview = () => {
   const { data, error, isLoading } = useGetOverviewQuery(undefined);
 
-  // console.log(data);
   const balanceData = useBalanceData(data?.balance);
   const potsData = usePotsData(data?.pots);
   const transactionsData = useTransactionsData(data?.transactions);
@@ -28,8 +27,8 @@ export const Overview = () => {
   return (
     <>
       {/* OVERVIEW HEADER */}
-      <h1 className="overview-title">Overview</h1>
-      <div className="overview-info-container">
+      <h1 className='overview-title'>Overview</h1>
+      <div className='overview-info-container'>
         <div>
           <h4>Current Balance</h4>
           <p>${balanceData.current}</p>
@@ -45,14 +44,14 @@ export const Overview = () => {
       </div>
 
       {/* OVERVIEW CONTENT */}
-      <section className="additionals-section">
+      <section className='additionals-section'>
         {/* LEFT SIDE OF THE OVERVIEW CONTENT (TOP ON MOBILE) */}
-        <div className="additionals-section-left">
+        <div className='additionals-section-left'>
           <OverviewPots potsData={potsData} />
           <OverviewTransactions transactionsData={transactionsData} />
         </div>
         {/* RIGHT SIDE OF THE OVERVIEW CONTENT (BOTTOM ON MOBILE) */}
-        <div className="additionals-section-right">
+        <div className='additionals-section-right'>
           <OverviewBudgets budgetData={budgetData} />
           <OverviewRecurring />
         </div>
