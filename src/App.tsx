8d353e98009ec0ad4e3d331bc63c84190309dfc4
Eddie Maceda <eddiemaceda11@gmail.com>
2025-04-15 22:03:40 -0700

    Include the Sidebar in the BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { Sidebar } from "./features/Sidebar/Sidebar";
-import { Overview } from "./features/Overview/Overview";
-import { Transaction } from "./features/Transactions/Transactions";
-import { Budgets } from "./features/Budgets/Budgets";
-import { Pots } from "./features/Pots/Pots";
-import { Recurring } from "./features/Recurring/Recurring";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { Sidebar } from './features/Sidebar/Sidebar';
+import { Overview } from './features/Overview/Overview';
+import { Transaction } from './features/Transactions/Transactions';
+import { Budgets } from './features/Budgets/Budgets';
+import { Pots } from './features/Pots/Pots';
+import { Recurring } from './features/Recurring/Recurring';
 
 function App() {
   return (
-    <div className="page-container">
-      <Sidebar />
-      <main>
-        <div className="main-container">
-          <BrowserRouter>
+    <div className='page-container'>
+      <BrowserRouter>
+        <Sidebar />
+        <main>
+          <div className='main-container'>
             <Routes>
               <Route
-                path="/"
+                path='/'
                 element={<Overview />}
               />
               <Route
-                path="/transactions"
+                path='/transactions'
                 element={<Transaction />}
               />
               <Route
-                path="/budgets"
+                path='/budgets'
                 element={<Budgets />}
               />
               <Route
-                path="/pots"
+                path='/pots'
                 element={<Pots />}
               />
               <Route
-                path="/recurring"
+                path='/recurring'
                 element={<Recurring />}
               />
             </Routes>
-          </BrowserRouter>
-        </div>
-      </main>
+          </div>
+        </main>
+      </BrowserRouter>
       {/* <Sidebar /> */}
     </div>
   );
