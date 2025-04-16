f51df85af0572e205e7b0d2d90345937174ee8b8
Eddie Maceda <eddiemaceda11@gmail.com>
2025-04-16 04:25:44 -0700

    Update formatting

--- src/features/Transactions/Transactions.tsx
+++ src/features/Transactions/Transactions.tsx
@@ -1,47 +1,47 @@
-import "./transactions.css";
+import './transactions.css';
 
 export const Transaction = () => {
   return (
     <>
-      <h1 className="transactions-title">Transactions</h1>
-      <div className="transactions-content-container">
+      <h1 className='transactions-title'>Transactions</h1>
+      <div className='transactions-content-container'>
         {/* TOP MENU */}
-        <div className="transactions-menu">
-          <div className="searchbar">
-            <input type="text" />
+        <div className='transactions-menu'>
+          <div className='searchbar'>
+            <input type='text' />
             <p>p</p>
           </div>
-          <div className="filters-container">
-            <div className="transactions_sort-by">
+          <div className='filters-container'>
+            <div className='transactions_sort-by'>
               <p>Sort by</p>
               <select
-                name=""
-                id=""
+                name=''
+                id=''
               >
-                <option value="latest">Latest</option>
-                <option value="oldest">Oldest</option>
-                <option value="az">A to Z</option>
-                <option value="za">Z to A</option>
-                <option value="highest">Highest</option>
-                <option value="lowest">Lowest</option>
+                <option value='latest'>Latest</option>
+                <option value='oldest'>Oldest</option>
+                <option value='az'>A to Z</option>
+                <option value='za'>Z to A</option>
+                <option value='highest'>Highest</option>
+                <option value='lowest'>Lowest</option>
               </select>
             </div>
-            <div className="transactions_categories">
+            <div className='transactions_categories'>
               <p>Category</p>
               <select>
-                <option value="all">All Transactions</option>
-                <option value="entertainment">Entertainment</option>
-                <option value="groceries">Groceries</option>
-                <option value="bills">Bills</option>
-                <option value="dining">Dining out</option>
-                <option value="personal">Personal care</option>
+                <option value='all'>All Transactions</option>
+                <option value='entertainment'>Entertainment</option>
+                <option value='groceries'>Groceries</option>
+                <option value='bills'>Bills</option>
+                <option value='dining'>Dining out</option>
+                <option value='personal'>Personal care</option>
               </select>
             </div>
           </div>
         </div>
 
         {/* TABLE HEADER */}
-        <div className="transactions_table-header">
+        <div className='transactions_table-header'>
           <p>Recipient/Sender</p>
           <p>Category</p>
           <p>Transaction Date</p>
@@ -49,18 +49,19 @@ export const Transaction = () => {
         </div>
 
         {/* TRANSACTIONS TABLE */}
-        <div className="transactions_table">
-          <div className="transactions_table--transaction">
-            <div className="recipient_sender">
+        <div className='transactions_table'>
+          {/* INDIVIDUAL TRANSACTION */}
+          <div className='transactions_table--transaction'>
+            <div className='recipient_sender'>
               <img
-                src=""
-                alt=""
+                src=''
+                alt=''
               />
               <p>Emma Richardson</p>
             </div>
-            <p className="transactions_category">General</p>
-            <p className="transaction_date">07 Feb 2025</p>
-            <p className="transaction_amount">+$75.50</p>
+            <p className='transactions_category'>General</p>
+            <p className='transaction_date'>07 Feb 2025</p>
+            <p className='transaction_amount'>+$75.50</p>
           </div>
         </div>
       </div>
