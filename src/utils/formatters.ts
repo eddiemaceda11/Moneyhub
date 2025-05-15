4bf26763a7198914de42050efaeedd3e13743e67
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-14 20:58:30 -0700

    Update the formatCurrency function to display USD symbol

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,5 +1,7 @@
 export function formatCurrency(value: string | number) {
   return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(Number(value));
