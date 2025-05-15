510514f33a4fec6b6f829ffc368b910e65addaf1
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-14 20:45:17 -0700

    Update the formatCurrency function to allow both string and number parameters

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,5 +1,5 @@
-export function formatCurrency(value: string) {
-  const formattedNumber = new Intl.NumberFormat("en-US", {
+export function formatCurrency(value: string | number) {
+  const formattedNumber = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(Number(value));
