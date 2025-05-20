10d0b95a2f84cda95c07ff22b69150a5c320e498
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-19 18:31:05 -0700

    Remove the additional dollar signs from the OverviewTransactions component

--- src/features/Overview/components/OverviewTransactions/IndividualTransaction.tsx
+++ src/features/Overview/components/OverviewTransactions/IndividualTransaction.tsx
@@ -5,32 +5,32 @@ export const IndividualTransaction = ({ transactionsData }: any) => {
   }
 
   function checkIfAmountIsNegative(amount: string) {
-    const firstCharOfString = amount.split("").shift();
-    if (firstCharOfString === "-") {
-      let modifiedAmount = amount.slice(0, 1) + "$" + amount.slice(1);
-      return <p className="negative-transaction">{modifiedAmount}</p>;
+    const firstCharOfString = amount.split('').shift();
+    if (firstCharOfString === '-') {
+      let modifiedAmount = amount.slice(0, 1) + amount.slice(1);
+      return <p className='negative-transaction'>{modifiedAmount}</p>;
     } else {
-      let modifiedAmount = "+$" + amount;
-      return <p className="positive-transaction">{modifiedAmount}</p>;
+      let modifiedAmount = '+' + amount;
+      return <p className='positive-transaction'>{modifiedAmount}</p>;
     }
   }
 
   return (
     <>
       <div>
-        <div className="oti-left">
+        <div className='oti-left'>
           <img
             src={configureImgPath(transactionsData.avatar)}
-            alt=""
+            alt=''
           />
           <p>{transactionsData.name}</p>
         </div>
-        <div className="oti-right">
+        <div className='oti-right'>
           {checkIfAmountIsNegative(transactionsData.amount)}
           <span>19 Aug 2024</span>
         </div>
       </div>
-      {transactionsData.name === "Urban Services Hub" ? "" : <div className="oti-line"></div>}
+      {transactionsData.name === 'Urban Services Hub' ? '' : <div className='oti-line'></div>}
     </>
   );
 };
