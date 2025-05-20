a6e4ec856a89613bef982bf0bc348675be683d2d
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-19 18:28:48 -0700

    Remove the additional dollar signs from the OverviewBudgets component

--- src/features/Overview/components/OverviewBudgets/OverviewBudgetsCategoryItem.tsx
+++ src/features/Overview/components/OverviewBudgets/OverviewBudgetsCategoryItem.tsx
@@ -1,10 +1,10 @@
 export const OverviewBudgetsCategoryItem = ({ budgetData }: any) => {
   return (
-    <div className="osbc-category-div">
-      <div className="osbc-category-left-border"></div>
-      <div className="osbc-category-container">
-        <div className="osbc-category">{budgetData.category}</div>
-        <div className="osbc-category-amount">${budgetData.maximum}</div>
+    <div className='osbc-category-div'>
+      <div className='osbc-category-left-border'></div>
+      <div className='osbc-category-container'>
+        <div className='osbc-category'>{budgetData.category}</div>
+        <div className='osbc-category-amount'>{budgetData.maximum}</div>
       </div>
     </div>
   );
