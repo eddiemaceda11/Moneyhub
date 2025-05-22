1229e31b4e1717dd8568c58cf25375c79440dee5
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-22 16:31:43 -0700

    Remove unused import

--- src/features/Transactions/utils/transactionsSelectedCategory.ts
+++ src/features/Transactions/utils/transactionsSelectedCategory.ts
@@ -1,5 +1,3 @@
-import { transactions } from '../transactionsData';
-
 export const selectedCategory = (category: string, transactions) => {
   console.log(category, transactions);
   const transactionsByCategory = transactions.filter((transaction) => transaction.category.toLowerCase() === category.toLowerCase());
