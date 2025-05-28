8e995b301d1077dee2dcd2f5e24b099fa23f3c0a
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-28 06:12:55 -0700

    Add a check for 'All Transaction'

--- src/features/Transactions/utils/transactionsSelectedCategory.ts
+++ src/features/Transactions/utils/transactionsSelectedCategory.ts
@@ -4,6 +4,9 @@ export const selectedCategory = (
   category: string,
   transactions: transaction[]
 ) => {
+  console.log(transactions);
+  console.log(category);
+  if (category === 'All Transactions') return transactions;
   const transactionsByCategory = transactions.filter(
     (transaction) =>
       transaction.category.toLowerCase() === category.toLowerCase()
