6b8741cff828482c3834952c27bff53ad33ce681
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-01 14:11:12 -0700

    Create a function to track for window resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { Sidebar } from './features/Sidebar/Sidebar';
+import { MobileSidebar } from './features/Sidebar/MobileSidebar';
 import { Overview } from './features/Overview/Overview';
 import { Transaction } from './features/Transactions/Transactions';
 import { Budgets } from './features/Budgets/Budgets';
 import { Pots } from './features/Pots/Pots';
 import { Recurring } from './features/Recurring/Recurring';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    console.log(windowSize.width);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const handleResize = () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    setWindowSize({ width: newWidth, height: newHeight });
+  };
+
+  console.log(windowSize.width);
+
   return (
     <div className='page-container'>
       <BrowserRouter>
-        <Sidebar />
+        {windowSize.width > 820 && <Sidebar />}
         <main>
           <div className='main-container'>
             <Routes>
@@ -38,8 +59,9 @@ function App() {
             </Routes>
           </div>
         </main>
+        {/* TODO - create a mobile sidebar */}
+        {windowSize.width <= 820 && <MobileSidebar />}
       </BrowserRouter>
-      {/* <Sidebar /> */}
     </div>
   );
 }
